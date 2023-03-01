6d6a7b380ea257c0e7c8364deca5db161d9a723a
Houssame Ait hsain <hossame@inbox.ru>
2023-03-01 22:51:04 +0100

    Create server.c

---
+++ server.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 19:33:34 by hait-hsa          #+#    #+#             */
+/*   Updated: 2023/01/15 14:46:04 by hait-hsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	handler(int signal, siginfo_t *info, void *newaction)
+{
+	static unsigned char	c;
+	static int				sigcount;
+	static int				correntpid;
+
+	if (info->si_pid != correntpid)
+	{
+		correntpid = info->si_pid;
+		sigcount = 0;
+	}
+	(void)newaction;
+	sigcount++;
+	c >>= 1;
+	if (signal == SIGUSR1)
+		c = c | 128;
+	if (sigcount == 8)
+	{
+		write(1, &c, 1);
+		c = 0;
+		sigcount = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	act;
+
+	write(1, "PID: ", 5);
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+	act.sa_sigaction = handler;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR2, &act, 0);
+	sigaction(SIGUSR1, &act, 0);
+	while (1)
+		pause();
+	return (0);
+}
