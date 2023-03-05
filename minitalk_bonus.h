e2e82531116078bde88de1383e65f36c0a1a14db
Houssame Ait hsain <hossame@inbox.ru>
2023-03-05 23:52:23 +0100

    Create minitalk_bonus.h

---
+++ minitalk_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 19:33:31 by hait-hsa          #+#    #+#             */
+/*   Updated: 2023/01/14 18:59:18 by hait-hsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+int		ft_atoi(const char *str);
+int		ft_putchar(char c);
+int		ft_putstr(const char *str);
+void	ft_putnbr(int n);
+#endif
